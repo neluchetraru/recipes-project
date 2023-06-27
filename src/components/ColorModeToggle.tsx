445c1f8c846417ea83fec1ce54186fb472fb5cce
Chetraru Ion <s204710@student.dtu.dk>
2023-06-27 17:17:50 +0200

    Create colorModeToggle component

---
+++ src/components/ColorModeToggle.tsx
@@ -0,0 +1,22 @@
+import { IconButton, useTheme } from "@mui/material";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import { ColorModeContext } from "../theme";
+import { CSSProperties, useContext } from "react";
+
+interface Props {
+  sx: CSSProperties;
+}
+export function ColorModeToggle({ sx }: Props) {
+  const theme = useTheme();
+  const colorMode = useContext(ColorModeContext);
+  return (
+    <IconButton sx={{ ...sx }} onClick={colorMode.toggleColorMode} color="info">
+      {theme.palette.mode === "dark" ? (
+        <Brightness7Icon />
+      ) : (
+        <Brightness4Icon />
+      )}
+    </IconButton>
+  );
+}
